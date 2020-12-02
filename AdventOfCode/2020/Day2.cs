3db5fb6e6b8c91403bca70900d2d77b7bbaf7a84
Jason Bush <jbush6@gmail.com>
2020-12-02 10:25:04 -0600

    Performance fixes day 2

--- AdventOfCode/2020/Day2.cs
+++ AdventOfCode/2020/Day2.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode._2020
 {
@@ -19,12 +17,18 @@ namespace AdventOfCode._2020
                 passwordCandidates.Add(new PasswordCandidate(int.Parse(line.Substring(0, (line.IndexOf('-')))), int.Parse(line.Substring((line.IndexOf('-') + 1), (line.IndexOf(' ') - line.IndexOf('-')))), line[line.IndexOf(':') - 1], line.Substring(line.IndexOf(':') + 2, (line.Length - line.IndexOf(':') - 2))));
             }
 
-            foreach(var candidate in passwordCandidates)
+            foreach (PasswordCandidate candidate in passwordCandidates)
             {
-                checkCount = candidate.password.Count(t => t == candidate.character);
+                checkCount = 0;
+                foreach(char c in candidate.password)
+                {
+                    if (c == candidate.character)
+                        checkCount++;
+                }
 
                 if (checkCount >= candidate.minCharCount && checkCount <= candidate.maxCharCount)
                     candidate.valid = true;
+
             }
 
             Console.WriteLine($"Part 1: {passwordCandidates.Count(t => t.valid == true)}");
@@ -32,14 +36,12 @@ namespace AdventOfCode._2020
 
         public void part2()
         {
-            foreach(var candidate in passwordCandidates)
+            foreach(PasswordCandidate candidate in passwordCandidates)
             {
-                if (candidate.password[candidate.minCharCount - 1] == candidate.character && candidate.password[candidate.maxCharCount - 1] == candidate.character)
-                    candidate.valid = false;
-                else if (candidate.password[candidate.minCharCount - 1] != candidate.character && candidate.password[candidate.maxCharCount - 1] != candidate.character)
-                    candidate.valid = false;
-                else
+                if (candidate.password[candidate.minCharCount - 1] == candidate.character ^ candidate.password[candidate.maxCharCount - 1] == candidate.character)
                     candidate.valid = true;
+                else
+                    candidate.valid = false;
             }
 
             Console.WriteLine($"Part 2: {passwordCandidates.Count(t => t.valid == true)}");
